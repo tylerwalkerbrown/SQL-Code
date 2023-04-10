e86f56c008b2cf1bad8e257b1457bace1324062b
Tyler Brown <94020684+tylerwalkerbrown@users.noreply.github.com>
2023-04-10 00:59:11 -0400

    Update HackerRank_SQL.sql

--- HackerRank_SQL.sql
+++ HackerRank_SQL.sql
@@ -43,6 +43,16 @@ select count(CITY) - count(distinct(CITY))
 from STATION S;
 
 
+#Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+
+(select CITY, length(CITY) as city_len 
+ from STATION 
+ order by city_len asc, CITY asc limit 1) 
+UNION
+(select CITY, length(city) as city_len 
+ from STATION 
+ order by city_len desc, CITY asc limit 1);
+
 
 
 
